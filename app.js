a0669b1f90938d4302d018b6ce134139ff149868
libiarubattinoportolan-ui <libiarubattinoportolan@gmail.com>
2026-07-27 19:42:56 -0300

    Primeira versão do MacroFlex Veg

---
+++ app.js
@@ -0,0 +1,33 @@
+// Meta diária
+
+const meta = {
+    kcal: 2200,
+        proteina: 190,
+            carbo: 200,
+                gordura: 60
+                };
+
+                // Valores consumidos
+
+                let consumido = {
+                    kcal: 0,
+                        proteina: 0,
+                            carbo: 0,
+                                gordura: 0
+                                };
+
+                                // Atualiza a tela
+
+                                function atualizarTela(){
+
+                                    document.getElementById("kcal").textContent =
+                                            consumido.kcal + " / " + meta.kcal + " kcal";
+
+                                                document.getElementById("proteina").textContent =
+                                                        consumido.proteina + " / " + meta.proteina + " g";
+
+                                                            document.getElementById("carbo").textContent =
+                                                                    consumido.carbo + " / " + meta.carbo + " g";
+
+                                                                        document.getElementById("gordura").textContent =
+                                                                                consumido.g
