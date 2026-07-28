2f1e3f633733e65ca73512312759200b571667b0
libiarubattinoportolan-ui <libiarubattinoportolan@gmail.com>
2026-07-27 21:08:38 -0300

    Atualizar o app.js

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-alert("JavaScript carregou!");
+document.addEventListener("DOMContentLoaded", function () {
+
+  const meta = {
+    kcal: 2200,
+    proteina: 190,
+    carbo: 200,
+    gordura: 60
+  };
+
+  const consumido = {
+    kcal: 0,
+    proteina: 0,
+    carbo: 0,
+    gordura: 0
+  };
+
+  document.getElementById("kcal").textContent =
+    consumido.kcal + " / " + meta.kcal + " kcal";
+
+  document.getElementById("proteina").textContent =
+    consumido.proteina + " / " + meta.proteina + " g";
+
+  document.getElementById("carbo").textContent =
+    consumido.carbo + " / " + meta.carbo + " g";
+
+  document.getElementById("gordura").textContent =
+    consumido.gordura + " / " + meta.gordura + " g";
+
+  document
+    .getElementById("novaRefeicao")
+    .addEventListener("click", function () {
+      alert("Nova Refeição funcionando! 🎉");
+    });
+
+});
